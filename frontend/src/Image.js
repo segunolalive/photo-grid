86d21205398970914e3fe410abd187a6f7fdb04a
Segun <segunolalive@gmail.com>
2018-10-02 09:47:05 +0100

    extract image component

---
+++ frontend/src/Image.js
@@ -0,0 +1,52 @@
+import React, { Component } from 'react';
+
+const div = {
+  display: 'flex',
+  position: 'relative',
+  justifyContent: 'center',
+  width: '50%',
+  height: '50%',
+  padding: 0,
+  margin: 0,
+  flexWrap: 'wrap',
+};
+
+const style = {
+  largeDiv: {
+    ...div,
+    width: '100%',
+    height: '100%'
+  },
+  div,
+  img: {
+    display: 'block',
+    width: '100%',
+    height: 'auto'
+  },
+  span: {
+    display: 'inline-flex',
+    alignSelf: 'flex-end',
+    justifySelf: 'flex-end',
+    background: 'rgba(0, 0, 0, 0.6)',
+    padding: '10px',
+    position: 'absolute',
+    width: '100%',
+    justifyContent: 'center',
+    color: 'white',
+  }
+};
+const Image = ({ incrementLikes, photo }) => {
+  return (
+    <figure style={photo.large ? style.largeDiv : style.div}>
+      <div className="overlay" onClick={event => incrementLikes(event, photo.id)}>
+        <div className="heart"></div>
+      </div>
+      <img src={photo.url} style={style.img} />
+      <figcaption style={style.span}>
+        Likes: {photo.likes}
+      </figcaption>
+    </figure>
+  );
+};
+
+export default Image;
